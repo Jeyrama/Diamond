1348de4685f87e1235220e315d04d081ef1e89f0
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-14 18:53:23 -0700

    Update Diamond.js

--- Diamond.js
+++ Diamond.js
@@ -43,3 +43,15 @@ function diamond (n) {
 }
 
 // or
+
+function diamond(n) {
+  if (n < 0 || n % 2 == 0) return null;
+  for (let i = 0, diamond = ''; i < n; i++) {
+    let stars = n - Math.abs(n - 2 * i - 1);
+    let spaces = (n - stars) / 2;
+    while (spaces-- > 0) diamond += ' ';
+    while (stars-- > 0) diamond += '*';
+    diamond += '\n';
+  }
+  return diamond;
+}
